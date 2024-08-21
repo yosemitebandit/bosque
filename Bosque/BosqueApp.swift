1c43d113ea463eec65ec51bf58c4702d91da43ce
Matt Ball <matt.ball.2@gmail.com>
2024-08-21 16:28:21 -0700

    Initial Commit

---
+++ Bosque/BosqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  BosqueApp.swift
+//  Bosque
+//
+//  Created by Matt Ball on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BosqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
